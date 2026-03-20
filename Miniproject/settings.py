9b77438ad7657148593dabdd3dd2ecf55062e7c3
Boopathi <boopathis.24chem@kongu.edu>
2026-03-20 20:51:03 +0530

    fixed BASE_DIR error

--- Miniproject/settings.py
+++ Miniproject/settings.py
@@ -9,6 +9,9 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+import os
+
+
 
 from pathlib import Path
 
@@ -27,10 +30,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-import os
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Application definition
 
 INSTALLED_APPS = [
@@ -119,7 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
